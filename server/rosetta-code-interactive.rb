2e8baab814eebee524b0b93d84e1903406edbbe0
Andrew Cole <aocole@gmail.com>
2013-04-09 18:20:50 -0700

    Added lang/task table and file browser

--- server/rosetta-code-interactive.rb
+++ server/rosetta-code-interactive.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby19
 require 'sinatra'
-require 'rdiscount'
 
 HOME = File.realpath(File.dirname(__FILE__) + '/..')
 ROSETTA = HOME + '/RosettaCode'
@@ -9,10 +8,18 @@ configure do
   set :public_folder, ROSETTA
 end
 
-get /(\/?)|(\/README.md)/ do
+get '/?' do
+  require 'rdiscount'
   if File.exist?(settings.public_folder + '/README.md')
     markdown :README, :views => settings.public_folder, :layout_engine => :erb
-  end
+  else
+    ''
+  end + table
+end
+
+get '/Lang/:lang/:task' do
+  @files = Dir.glob(ROSETTA + '/' + File.join('Lang', params[:lang], params[:task], '*')).collect{|f|f.sub(/^#{ROSETTA}/,'')}.sort
+  erb :language_task
 end
 
 template :layout do
@@ -31,3 +38,23 @@ template :layout do
   </html>
 TEMPLATE
 end
+
+def langs
+  @langs ||= begin
+    Dir.glob(ROSETTA + '/Lang/*/').collect do |name|
+      File.basename name
+    end
+  end.sort
+end
+
+def tasks
+  @tasks ||= begin
+    Dir.glob(ROSETTA + '/Task/*/').collect do |name|
+      File.basename(name)
+    end
+  end.sort
+end  
+
+def table
+  erb :table
+end 
